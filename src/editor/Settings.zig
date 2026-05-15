485a0a404db8aaa402e759f47054753889b41088
foxnne <foxnne@gmail.com>
2026-05-15 15:22:33 -0500

    Change default to .auto

--- src/editor/Settings.zig
+++ src/editor/Settings.zig
@@ -43,9 +43,8 @@ min_window_size: [2]f32 = .{ 640, 480 },
 initial_window_size: [2]f32 = .{ 1280, 720 },
 
 /// Zoom/pan control scheme (`auto` picks mouse vs trackpad gestures from `dvui.getMouseTypeHint` after scroll events).
-/// Default chosen by platform: trackpad on macOS (most laptops use one by default),
-/// mouse elsewhere. Users can switch to `.auto` or the other explicit value in settings.
-input_scheme: InputScheme = if (builtin.os.tag == .macos) .trackpad else .mouse,
+/// FIXME: On macOS, .auto always reports trackpad as scroll events are not reliably different from trackpad events.
+input_scheme: InputScheme = .auto,
 
 /// Whether or not to show rulers on each canvas.
 show_rulers: bool = true,
